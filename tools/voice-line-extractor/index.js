f2900e1844a2640a7d956178edb779dcfef7bbc1
Broderick Young <bxyoung89@gmail.com>
2020-05-01 22:26:14 -0500

    first round of filtering.

--- tools/voice-line-extractor/index.js
+++ tools/voice-line-extractor/index.js
@@ -11,9 +11,39 @@ const OUTPUT_PATH = path.join(__dirname, '../output');
 const OUTPUT_FILE = path.join(OUTPUT_PATH, './voicelines.csv');
 const LANGUAGE_TO_EXTRACT = "English";
 
+const fileFilters = [
+  file => file.indexOf('StoryMode') !== -1,
+  file => file.indexOf('SandBoxCore') !== -1,
+  file => file.indexOf('std_trait_strings_xml.xml') !== -1,
+  file => file.indexOf('std_TaleWorlds_MountAndBlade_CustomBattle.xml') !== -1,
+  file => file.indexOf('_ViewModelCollection.xml') !== -1,
+  file => file.indexOf('std_TaleWorlds_CampaignSystem.xml') !== -1,
+  file => file.indexOf('_View.xml') !== -1,
+  file => file.indexOf('_GauntletUI.xml') !== -1,
+  file => file.indexOf('std_spworkshops_xml.xml') !== -1,
+  file => file.indexOf('std_spspecialcharacters_xml.xml') !== -1,
+  file => file.indexOf('std_spprojects_xml.xml') !== -1,
+  file => file.indexOf('std_spclans_xml.xml') !== -1,
+  file => file.indexOf('std_settlements_xml.xml') !== -1,
+  file => file.indexOf('std_crafting_pieces_xml.xml') !== -1,
+  file => file.indexOf('std_global_strings_xml.xml') !== -1,
+  file => file.indexOf('std_item_modifiers_xml.xml') !== -1,
+  file => file.indexOf('std_mpbadges_xml.xml') !== -1,
+  file => file.indexOf('std_mpcharacters_xml.xml') !== -1,
+  file => file.indexOf('std_mpclassdivisions_xml.xml') !== -1,
+  file => file.indexOf('std_mpcultures_xml.xml') !== -1,
+  file => file.indexOf('std_mpitems_xml.xml') !== -1,
+  file => file.indexOf('std_multiplayer_strings_xml.xml') !== -1,
+  file => file.indexOf('std_native_strings_xml.xml') !== -1,
+  file => file.indexOf('std_TaleWorlds_Core.xml') !== -1,
+  file => file.indexOf('std_TaleWorlds_MountAndBlade.xml') !== -1,
+  file => file.indexOf('std_TaleWorlds_MountAndBlade_Diamond_Server.xml') !== -1,
+];
+
 const xmlFiles = readDirRecursive(PATH_TO_BANNERLORD)
     .map(name => path.join(PATH_TO_BANNERLORD, name))
-    .filter(file => file.endsWith('.xml'));
+    .filter(file => file.endsWith('.xml'))
+    .filter(file => !fileFilters.some(filter => filter(file)));
 
 const getXmlFileAsJson = (xmlFile) => {
     const fileContents = `${fs.readFileSync(xmlFile)}`;
